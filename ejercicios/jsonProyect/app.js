04796c2de009b14cb524391673f2b6a4c7ec4fd9
David <davidchengzijie@gmail.com>
2021-11-29 21:04:27 +0100

    clase de 29/11

---
+++ ejercicios/jsonProyect/app.js
@@ -0,0 +1,34 @@
+/**
+ * 
+ * https://jsonplaceholder.typicode.com/users
+ * Crear una tabla dinamica que almacene por filas el nombre y el apellido, el email y la geolocalización
+ * de todos los usuarios de ese fichero json.
+ * Crear una cookie que contenga el nº de usuarios que he insertado en la tabla.
+ * Crear utilizanco web storage una clave que contenga el código de colores asignado a vuestra página web
+ * y a la tabla
+ */
+
+
+const JSONData = 
+'[{"id":1,"name":"Leanne Graham","username":"Bret"},{"id":2,"name":"Lincolm","username":"Abraham"}]';
+
+const objData = JSON.parse(JSONData);
+
+const tabla = document.querySelector("table");
+
+
+for(let i = 0; i < objData.length; i++) {
+    let fila = document.createElement("tr");
+    for(let key in objData[i]) {
+        let columna = document.createElement("td");
+        let texto = document.createTextNode(objData[i][key]);
+        columna.appendChild(texto);
+        fila.appendChild(columna);
+    }
+    tabla.appendChild(fila);
+}
+
+
+
+
+
